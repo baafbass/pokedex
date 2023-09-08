b153c7409c0e23d9f4159fdc14dd0a74d7fbc170
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-08 18:03:04 +0300

    Images With Python 3

--- images.py
+++ images.py
@@ -1,15 +1,7 @@
 from PIL import Image,ImageFilter
 
-img = Image.open('./Pokedex/pikachu.jpg')
-filtered_img = img.convert('L')
-crooked = filtered_img.rotate(90)
-crooked.save("rotetedPikachu.png","png")
-resized = filtered_img.resize((300,300))
-resized.save("resizedPikachu.png","png")
-resized.show()
-filtered_img.save('grayPikachu.png','png') # I converted the images to .png because it support the above filters, we may get errors using the jpg format
-filtered_img.show() # it will show us the image
-
-box=(100,100,400,400)
-region = filtered_img.crop(box)
-filtered_img.save("CroppedPikachu.png",'png')
+img = Image.open('./astro.jpg')
+new_img = img.resize((400,400))
+new_img.save("resizethumbnail.jpg")
+img.thumbnail((400,200))
+img.save("thumbnailledAstro.jpg")
